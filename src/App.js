49fc1f61b52fc45bd89d54aa5fc2ff530f406d9e
bharatpandey30 <bharatpandey361@gmail.com>
2023-12-30 12:37:12 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,34 @@
+// app.js
+import React from 'react';
 
-import './App.css';
-import React, {Component} from 'react';
-import Parent from './Component/Parent';
-function App(){
-  return(
-    <div className='App'>
-      <Parent/>
-    </div>
+import Greet from './Component/Greet';
+
+const Button = ({ children, backgroundColor }) => {
+  return <button style={{ backgroundColor }}>{children}</button>;
+};
+
+const Alert = ({ children }) => {
+  return (
+    <>
+      <div className="Overlay" />
+      <div className="Alert">{children}</div>
+    </>
   );
 };
+
+const DeleteButton = () => {
+  return <Button backgroundColor="red">Delete</Button>;
+};
+
+function App() {
+  return (
+    <div className="App">
+      <Alert>
+        <DeleteButton />
+        <Greet />
+      </Alert>
+    </div>
+  );
+}
+
 export default App;
